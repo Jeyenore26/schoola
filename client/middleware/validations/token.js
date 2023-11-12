57e9c640f2ceba1231fa9eb976d79b2a7602591f
DrTr4p <65681890+kasdo770@users.noreply.github.com>
2023-11-13 00:18:35 +0200

    312

--- client/middleware/validations/token.js
+++ client/middleware/validations/token.js
@@ -3,9 +3,11 @@ const jwt = require("jsonwebtoken")
 
 export function validateToken(headersList) {
 
-
+    console.log(headersList)
 
     const auth = headersList.get('authorization')
+
+    console.log(auth)
 
     const user = jwt.verify(auth, process.env.SECRET_TOKEN)
 
